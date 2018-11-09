a1d6bfd9318a7205fe4d691c9af3781776213fd3
melonsad <43734300+melonsad@users.noreply.github.com>
2018-11-09 14:52:34 +0900

    Update main.py

--- main.py
+++ main.py
@@ -14,9 +14,14 @@ label1.place(x=138, y=50)
 label2=Label(window, text="최고점수")
 label2.place(x=195, y=300)
 
+score = open('scoreboard.txt','r')
+rank = score.readlines()
+rank.sort()
+rank.reverse()
 scorelabel =Label(window, text=rank)
 scorelabel.place(x=210,y=320)
 
 btn1=Button(window,text="게임시작")
 btn1.place(x=380, y=783)
 
+score = 0
