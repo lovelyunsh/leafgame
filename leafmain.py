1f86a0bfe42cb46d957bf635ca89037c6189885d
lovelyunsh <43734328+lovelyunsh@users.noreply.github.com>
2018-11-12 20:43:18 +0900

    getrank() 수정
    { } 뜨던거 수정
    scorelabel 독립적으로 두고 게임 종료되면 config()로 refresh하게 만듦

--- leafmain.py
+++ leafmain.py
@@ -8,6 +8,7 @@ import subprocess
 def gamestart():
   subprocess.call("leafgame.py", shell=True)
   getrank()
+  scorelabel.config(text = rankstr)
     
 window=Tk()
 window.title("조선대학교 108계단 오르기")
@@ -19,18 +20,22 @@ label1.place(x=138, y=50)
 label2=Label(window, text="최고점수")
 label2.place(x=195, y=300)
 
+rankstr = ''
 def getrank():
+  global rankstr
+  rankstr = ''
   score = open('scoreboard.txt','r')
   rank = score.readlines()
   score.close()
   rank = [int(i) for i in rank]
   rank.sort()
   rank.reverse()
-  rank = [(str(i) + '\n') for i in rank]
-  scorelabel =Label(window, text=rank)
-  scorelabel.place(x=210,y=320)
-  
-getrank()
+  for i in range(0 , 9) :
+    rankstr = rankstr+str(rank[i]) + "\n"
+
+getrank()   
+scorelabel =Label(window, text=rank)
+scorelabel.place(x=210,y=320)
 
 btn1=Button(window,text="게임시작")
 btn1.place(x=380, y=783, command = gamestart)
